f7e916e99e5cee209aa6a76be9a92d54ea8f3bba
Channakeshavapr <channakeshavapr@gmail.com>
2024-06-11 20:22:32 +0530

    HeroText file added

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,27 +1,23 @@
 import React from "react";
 import classes from "./Service.module.css";
+import Downarraw from "../assests/downarrow.jpg";
 
 const Service = () => {
   return (
-    <div>
-      <section className={classes.heading1}>
-        <p>Skills & Service</p>
-      </section>
-      <ul className={classes.para}>
-        <li className={classes.para1}>
-          <p>
-            We offer cutting-edge software solutions that streamline business
-            operations and enhance productivity. Our expert web design services
-            create visually stunning and user-friendly websites tailored to your
-            brand
-          </p>
-        </li>
-
-        <li className={classes.para2}>
-          <p>What can I offer you?</p>
-          <p>Lets's figure it out</p>
-        </li>
-      </ul>
+    <div className={classes.container}>
+      <div className={classes["left-paragraph"]}>
+        <h2>Skills & Service</h2>
+        <p>
+          We offer cutting-edge software solutions that streamline business
+          operations and enhance productivity. Our expert web design services
+          create visually stunning and user-friendly websites tailored to your
+          brand.
+        </p>
+      </div>
+      <div className={classes["right-paragraph"]}>
+        <p>What can I offer you?</p>
+        <p>Lets's figure it out</p>
+      </div>
     </div>
   );
 };
