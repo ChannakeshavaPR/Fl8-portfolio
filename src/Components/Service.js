3c96643b7900a5725949fb8531c1680fcdfa4df6
Channakeshavapr <channakeshavapr@gmail.com>
2024-06-12 20:57:58 +0530

    servicesUpdated

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,24 +1,97 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classes from "./Service.module.css";
-import Downarraw from "../assests/downarrow.jpg";
 
 const Service = () => {
   return (
-    <div className={classes.container}>
-      <div className={classes["left-paragraph"]}>
-        <h2>Skills & Service</h2>
-        <p>
-          We offer cutting-edge software solutions that streamline business
-          operations and enhance productivity. Our expert web design services
-          create visually stunning and user-friendly websites tailored to your
-          brand.
-        </p>
+    <Fragment>
+      <div className={classes.container}>
+        <div className={classes["left-paragraph"]}>
+          <h2>Skills & Service</h2>
+          <p>
+            We offer cutting-edge software solutions that streamline business
+            operations and enhance productivity. Our expert web design services
+            create visually stunning and user-friendly websites tailored to your
+            brand.
+          </p>
+        </div>
+        <div className={classes["right-paragraph"]}>
+          <p>What can I offer you?</p>
+          <p>Lets's figure it out</p>
+        </div>
       </div>
-      <div className={classes["right-paragraph"]}>
-        <p>What can I offer you?</p>
-        <p>Lets's figure it out</p>
+      <div className={classes.container}>
+        <div className={classes["left-paragraphs"]}>
+          <h2>Web designs</h2>
+          <p>
+            ontrary to popular belief, Lorem Ipsum is not simply random text. It
+            has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </p>
+        </div>
+        <div className={classes["right-paragraphs"]}>
+          <h2>Software Solutions</h2>
+          <p>
+            ontrary to popular belief, Lorem Ipsum is not simply random text. It
+            has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </p>
+        </div>
       </div>
-    </div>
+      <div className={classes.container}>
+        <div className={classes["left-paragraphs"]}>
+          <h2>Logo Design</h2>
+          <p>
+            ontrary to popular belief, Lorem Ipsum is not simply random text. It
+            has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </p>
+        </div>
+        <div className={classes["right-paragraphs"]}>
+          <h2>Lading Pages</h2>
+          <p>
+            ontrary to popular belief, Lorem Ipsum is not simply random text. It
+            has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </p>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
